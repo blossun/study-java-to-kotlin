d2ca133fd5a83466666726e6baf161454b895ae1
blossun <blossunn@gmail.com>
2023-04-21 20:49:51 +0900

    5강. 사칙연산 계산기의 나눗템 테스트 작성
    수동으로 만든 테스트 코드의 단점
    1. 테스트 클래스와 메소드가 생길 때마다 메인 메소드에 수동으로 코드를 작성해주어야 하고, 메인 메소드가 아주 커진다.
    테스트 메소드를 개별적으로 실행하기도 어렵다.
    2. 테스트가 실패한 경우 무엇을 기대하였고, 어떤 잘못된 값이 들어와 실패했는지 알려주지 않는다.
    예외를 던지거나, try catch를 사용해야 하는 등 직접 구현해야 할 부분이 많아 불편하다.
    
    3. 테스트 메소드별로 공통적으로 처리해야 하는 기능이 있다면, 메소드마다 중복이 생긴다.
    
    이러한 단점을 극복하지 위해 Junit 5를 사용해보자

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,13 +1,17 @@
 package com.group.libraryapp.calculator
 
 import org.junit.jupiter.api.Assertions.*
+import java.lang.IllegalArgumentException
 
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
+
 class CalculatorTest {
     // test 결과 문제가 없다면 정상적으로 실행 후 종료되고, 문제가 있다면 Exception이 발생한다.
     fun addTest() {
@@ -47,6 +51,41 @@ class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalStateException()
         }
+    }
+
+    // 0이 아닌 수로 나눈 경우
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    // 0으로 나눈 경우 Exception이 발생한다.
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            // 테스트 성공!
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다.")
+            }
+            return
+
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 Exception이 발생하지 않음")
     }
 
 }
