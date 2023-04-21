bba5ce6d01e6ea12520a1408e009c4ed2af2e3e6
blossun <blossunn@gmail.com>
2023-04-21 20:41:56 +0900

    4강. 사칙연산 계산기에 대해 테스트 코드 작성하기 (4) 테스트 코드 추가
    코드 간결함을 위해 public setter을 열어두되 사용하지 않는 것을 컨벤션으로 하고 실습한다.

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -3,28 +3,25 @@ package com.group.libraryapp.calculator
 import java.lang.IllegalArgumentException
 
 class Calculator(
-    private var _number: Int
+    var number: Int
 ) {
 
-    val number: Int
-        get() = this._number //내부 _number 값을 조회할 수 있는 public getter만 열어준다.
-
     fun add(operand: Int) {
-        this._number += operand
+        this.number += operand
     }
 
     fun minus(operand: Int) {
-        this._number -= operand
+        this.number -= operand
     }
 
     fun multiply(operand: Int) {
-        this._number *= operand
+        this.number *= operand
     }
 
     fun divide(operand: Int) {
         if (operand == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다.")
         }
-        this._number /= operand
+        this.number /= operand
     }
 }
--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -5,15 +5,48 @@ import org.junit.jupiter.api.Assertions.*
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
 }
 class CalculatorTest {
     // test 결과 문제가 없다면 정상적으로 실행 후 종료되고, 문제가 있다면 Exception이 발생한다.
     fun addTest() {
+        // given - test를 위한 준비
         val calculator = Calculator(5)
+
+        // when - test하려는 기능을 호출
         calculator.add(3)
 
+        // then - 호출 결과를 검증
         if (calculator.number != 8) {
             throw IllegalStateException()
         }
     }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
 }
