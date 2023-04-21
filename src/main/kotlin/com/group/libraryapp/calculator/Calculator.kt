75980e3af9164c058d0474a580cb9a8b7a056bb3
blossun <blossunn@gmail.com>
2023-04-21 20:38:22 +0900

    4강. 사칙연산 계산기에 대해 테스트 코드 작성하기 (3) backing field 사용하기
    내부적으로 사용할 backing field를 선언하고, 외부에서는 public getter로 값을 조회할 수 있도록 한다.

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -3,25 +3,28 @@ package com.group.libraryapp.calculator
 import java.lang.IllegalArgumentException
 
 class Calculator(
-    var number: Int
+    private var _number: Int
 ) {
 
+    val number: Int
+        get() = this._number //내부 _number 값을 조회할 수 있는 public getter만 열어준다.
+
     fun add(operand: Int) {
-        this.number += operand
+        this._number += operand
     }
 
     fun minus(operand: Int) {
-        this.number -= operand
+        this._number -= operand
     }
 
     fun multiply(operand: Int) {
-        this.number *= operand
+        this._number *= operand
     }
 
     fun divide(operand: Int) {
         if (operand == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다.")
         }
-        this.number /= operand
+        this._number /= operand
     }
 }
