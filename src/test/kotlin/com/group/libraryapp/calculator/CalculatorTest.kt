0e921a694a3310b604ff352cb5f29d7a66d0bb36
blossun <blossunn@gmail.com>
2023-04-21 20:31:56 +0900

    4강. 사칙연산 계산기에 대해 테스트 코드 작성하기 (1) data class 사용하기

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,20 @@
+package com.group.libraryapp.calculator
+
+import org.junit.jupiter.api.Assertions.*
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+class CalculatorTest {
+    // test 결과 문제가 없다면 정상적으로 실행 후 종료되고, 문제가 있다면 Exception이 발생한다.
+    fun addTest() {
+        val calculator = Calculator(5)
+        calculator.add(3)
+
+        val expectedCalculator = Calculator(8)
+        if (calculator != expectedCalculator) {
+            throw IllegalStateException()
+        }
+    }
+}
