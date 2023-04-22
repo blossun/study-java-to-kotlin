f487b034a91ea235c6296f3be2a0e7b40fdd39fb
blossun <blossunn@gmail.com>
2023-04-22 22:01:05 +0900

    17강. Optional 제거하기(4) - CrudRepositoryExtension.kt 확장함수 이용하기

--- src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
+++ src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
@@ -1,5 +1,13 @@
 package com.group.libraryapp.util
 
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
 fun fail(): Nothing {
     throw IllegalArgumentException()
+}
+
+// 참고 - CrudRepositoryExtensions.kt
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T { // null일 경우 Exception을 던질 것이기 때문에 nullable한 리턴값을 쓰지 않는다.
+    return this.findByIdOrNull(id) ?: fail()
 }
